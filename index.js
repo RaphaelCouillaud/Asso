69e9dcccfd9f9d3e6b33ec716f7dc8b65d43334e
RaphaelCouillaud <couillaud.raphael@gmail.com>
2021-05-18 01:40:16 +0200

    menu toggle animation + countdown founction

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let header = document.getElementById('header');
 let text = document.getElementById('text');
 let bird1 = document.getElementById('bird1');
 let bird2 = document.getElementById('bird2');
-let cloud1 = document.getElementById('cloud1');
-let cloud2 = document.getElementById('cloud2');
-let sun = document.getElementById('sun');
+//let cloud1 = document.getElementById('cloud1');
+//let cloud2 = document.getElementById('cloud2');
+//let sun = document.getElementById('sun');
 let back = document.getElementById('back');
-let btn = document.getElementById('btn');
-let plants = document.getElementById('plants');
-let turtle = document.getElementById('turtle');
-let fish = document.getElementById('fish');
+let actioncounter = document.getElementById('actioncounter');
+//let plants = document.getElementById('plants');
+//let turtle = document.getElementById('turtle');
+//let fish = document.getElementById('fish');
 let piles1 = document.getElementById('piles1');
 let piles2 = document.getElementById('piles2');
 let water = document.getElementById('water');
@@ -23,22 +23,56 @@ window.addEventListener('scroll', () => {
  bird1.style.left = value * -2 + 'px';
  bird2.style.top = value * -1.5 + 'px';
  bird2.style.left = value * 5 + 'px';
- cloud1.style.left = value * -2 + 'px';
- cloud2.style.left = value * 5 + 'px';
- sun.style.left = value * -0.5 + 'px';
- sun.style.top = value * 0.25 + 'px';
- btn.style.marginTop = value * 0.5 + 'px';
+ //cloud1.style.left = value * -2 + 'px';
+ //cloud2.style.left = value * 5 + 'px';
+ //sun.style.left = value * -0.5 + 'px';
+ //sun.style.top = value * 0.25 + 'px';
+ actioncounter.style.marginTop = value * 0.5 + 'px';
  back.style.top = value * -0.15 + 'px';
- plants.style.top = value * 0.025 + 'px';
- fish.style.left = value * 0.55 + 'px';
- turtle.style.left = value * -0.5 + 'px';
+ //plants.style.top = value * 0.025 + 'px';
+ //fish.style.left = value * 0.55 + 'px';
+ //turtle.style.left = value * -0.5 + 'px';
  piles1.style.left = value * -3 + 'px';
  piles2.style.left = value * 3 + 'px';
  
  });
+//MENU BURGER ANIMATION //
+const menuAnim = document.querySelector('.menu-toggle');
+let openMenu = false;
+menuAnim.addEventListener('click', () => {
+	if(!openMenu) {
+		menuAnim.classList.add('open');
+		openMenu = true;
+	} else {
+		menuAnim.classList.remove('open');
+		openMenu = false;
+	}
+});
 
-//COUNDOWN//////////////////////////////////////////////////////////
+//COUNTDOWN//////////////////////////////////////////////////////////
+let countDate = new Date('June 5, 2021 14:30:00').getTime();
+function newYear(){
+	let currentTime = new Date().getTime();
+	let gapTime = countDate - currentTime;
+		let seconds = 1000;
+		let minutes = seconds * 60;
+		let hours = minutes * 60;
+		let days = hours * 24;
 
+	let daysCount = Math.floor(gapTime / (days));
+	let hoursCount = Math.floor((gapTime % (days)) / (hours));
+	let minutesCount = Math.floor((gapTime % (hours)) / (minutes));
+	let secondsCount = Math.floor((gapTime % (minutes)) / seconds);
+
+	document.getElementById('days').innerText = daysCount;
+	document.getElementById('hours').innerText = hoursCount;
+	document.getElementById('minutes').innerText = minutesCount;
+	document.getElementById('seconds').innerText = secondsCount;
+}
+setInterval (function(){
+	newYear();
+},1000)
+//////
 
 
 // SCROLL TO TOP //
