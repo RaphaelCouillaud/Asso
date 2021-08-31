b16d64f1fe832107950ca268c243bb74e8ff57bf
RaphaelCouillaud <couillaud.raphael@gmail.com>
2021-08-31 23:27:47 +0200

    august refresh

--- soutiens.js
+++ soutiens.js
@@ -130,7 +130,19 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function (revealTitle) {
 	observerIntersect.observe(revealTitle)
 })
 ///////////////////////////////////////////////////////////////
+var loadTimeOut = function()
+{
+  var soundWave = $('.sound-wave .bar').removeClass('bar')
+  setTimeout(
+    function(){ 
+      alert("Hello"); 
+    }, 3000
+  );
+}
 
+$('.sound-wave .bar').addClass('bar2');
+
+// loadTimeOut();
 
 
 // SCROLL TO TOP //
