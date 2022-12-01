5f40cf2b9f4261da194b999069b881638d2e8f14
RaphaelCouillaud <couillaud.raphael@gmail.com>
2022-12-01 17:16:58 +0100

    4dec

--- actus.js
+++ actus.js
@@ -85,7 +85,7 @@ menuAnim.addEventListener('click', () => {
 });
 
 //COUNTDOWN//////////////////////////////////////////////////////////
-let countDate = new Date('November 27, 2022 10:00:00').getTime();
+let countDate = new Date('December 04, 2022 09:30:00').getTime();
 function newYear(){
 	let currentTime = new Date().getTime();
 	let gapTime = countDate - currentTime;
