24f7e084d9f6afc889851c16e88fc1557d221d76
RaphaelCouillaud <couillaud.raphael@gmail.com>
2023-02-26 22:50:06 +0100

    baies et bilan

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ menuAnim.addEventListener('click', () => {
 });
 
 //COUNTDOWN//////////////////////////////////////////////////////////
-let countDate = new Date('February 12, 2023 10:00:00').getTime();
+let countDate = new Date('March 26, 2023 14:00:00').getTime();
 function newYear(){
 	let currentTime = new Date().getTime();
 	let gapTime = countDate - currentTime;
