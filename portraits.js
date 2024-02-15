11fa4064e59885a0761f00895b996ef7db0b2531
RaphaelCouillaud <couillaud.raphael@gmail.com>
2024-02-15 20:09:44 +0100

    bilan st froult ok

--- portraits.js
+++ portraits.js
@@ -85,7 +85,7 @@ menuAnim.addEventListener('click', () => {
 });
 
 //COUNTDOWN//////////////////////////////////////////////////////////
-let countDate = new Date('February 11, 2024 10:00:00').getTime();
+let countDate = new Date('March 17, 2024 15:00:00').getTime();
 function newYear(){
 	let currentTime = new Date().getTime();
 	let gapTime = countDate - currentTime;
